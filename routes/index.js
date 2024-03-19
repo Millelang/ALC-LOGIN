1b09c0f7adf31fa192d7c0cef279dc4a6a653849
millelang <milton.langstrom@elev.ga.ntig.se>
2024-03-19 10:32:59 +0100

    milton

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const { body ,matchedData , validationResult } = require('express-validator')
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
@@ -9,7 +9,8 @@ const pool = require('../db.js')
 router.get('/', function (req, res) {
   res.render('index.njk', {
     title: 'Hej user',
-    message: 'Välkommen'
+    message: 'Välkommen',
+    username: req.session.username
   })
 })
 
@@ -20,6 +21,18 @@ router.get('/hashTest', async function (req, res) {
   })
 })
 
+
+router.post('/signup', async function (req, res) {
+  const username = req.body.username
+  const password = req.body.password
+  
+  bcrypt.hash(password, 10, function (err, hash) {
+    encrypted = hash
+    return encrypted
+})
+
+
+})
 
 router.post('/login', async function (req, res) {
 
@@ -54,14 +67,16 @@ router.post('/login', async function (req, res) {
 router.get('/login', function (req, res) {
   res.render('login.njk', {
     title: 'Hej user',
-    message: 'Välkommen'
+    message: 'Välkommen',
+    username: req.session.username
   })
 })
 
 router.get('/signup', function (req, res) {
   res.render('signup.njk', {
     title: 'Hej user',
-    message: 'Välkommen'
+    message: 'Välkommen',
+    username: req.session.username
   })
 })
 
