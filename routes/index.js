655ed66698a40aa74049d2be6b18503bfdc21c22
Gabriel Nilsson Högberg <gabrielnilsson.hogb@elev.ga.ntig.se>
2024-03-19 10:02:39 +0100

    routes, server and view changes

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,14 @@ const saltRounds = 10;
 const pool = require('../db.js')
 
 router.get('/', function (req, res) {
-  res.render('index.njk', { 
+  res.render('index.njk', {
     title: 'Hej user',
     message: 'Välkommen'
   })
 })
 
 router.get('/hashTest', async function (req, res) {
-  bcrypt.hash('gligon123', 10, function(err, hash) {
+  bcrypt.hash('gligon123', 10, function (err, hash) {
     console.log(hash)
     return res.json(hash);
   })
@@ -30,51 +30,60 @@ router.post('/login', async function (req, res) {
   )
 
   const passwordenter = req.body.password
- 
-    bcrypt.compare(passwordenter, userpassword[0].password, function(err, result) {
-      if(result){
-        res.json({message: 'Du är inloggad'})
-      } else {
-        res.json({message: 'Fel lösenord'})
-      }
-    });
+
+  bcrypt.compare(passwordenter, userpassword[0].password, function (err, result) {
+    req.session.username = username
+    // req.session.login = false
+    if (result) {
+      req.session.login = true
+      res.redirect('/minasidor')
+      console.log(req.session.login)
+    } else {
+      res.json({ message: 'Fel lösenord' })
+    }
+  });
 
 
-  
+
 
   // const  password = req.body.password
 
-//SELECT * FROM salam_login WHERE id = 1
+  //SELECT * FROM salam_login WHERE id = 1
 })
 
 router.get('/login', function (req, res) {
-  res.render('login.njk', { 
+  res.render('login.njk', {
     title: 'Hej user',
     message: 'Välkommen'
   })
 })
 
 router.get('/signup', function (req, res) {
-  res.render('signup.njk', { 
+  res.render('signup.njk', {
     title: 'Hej user',
     message: 'Välkommen'
   })
 })
 
 router.get('/minasidor', function (req, res) {
-  res.render('minasidor.njk', { 
-    title: 'Hej user',
-    message: 'Välkommen'
-  
-})
+  console.log(req.session)
+  if (req.session.login) {
+    res.render('minasidor.njk', {
+      username: req.session.username,
+      message: 'Välkommen'
+    })
+  } else {
+    res.redirect('/login')
+  }
+
 })
 
-router.get('/test', async function  (req, res) {
-  
-    const [result] = await pool.promise().query(
-      `SELECT * FROM salam_login `,
-    )
- 
+router.get('/test', async function (req, res) {
+
+  const [result] = await pool.promise().query(
+    `SELECT * FROM salam_login `,
+  )
+
   res.json(result)
 })
 
